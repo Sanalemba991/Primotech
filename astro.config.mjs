4eb943d412b7eefa57dd9f918e0df03a21af37a2
Sanalemba991 <your-new-email@gmail.com>
2025-06-30 17:49:01 +0530

    30 Mon 2025 end of june

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react'; // ✅ Add this line
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://primotechllc.com',
-	integrations: [
-		tailwind(),
-	],
+    integrations: [
+        tailwind(),
+        react(), // ✅ Add this line
+    ],
 });
---
+++ src/components/Whatsapp.tsx
@@ -0,0 +1,16 @@
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
+const Whatsapp = () => {
+    return (
+      
+            <FloatingWhatsApp  
+                accountName='PrimoTech' 
+                phoneNumber='+971552929644' 
+                statusMessage="Live chat now"
+                chatMessage="Welcome to PrimoTech 🤝. How can we help?"
+            />
+    
+    )
+}
+
+export default Whatsapp;
