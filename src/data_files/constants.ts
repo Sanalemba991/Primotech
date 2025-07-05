10deb34b2c8d1e44422724ff2b67883d79c10cc3
Sanalemba991 <your-new-email@gmail.com>
2025-07-05 10:57:36 +0530

    okay

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,36 +1,36 @@
 
 export const SITE = {
 	Title: "Top Wholesale Deals on Uniarch Security Products",
-	baseURL:"https://lovosis.com" ,
-	canonical: "https://lovosis.com", 
+	baseURL:"https://primotechllc.com" ,
+	canonical: "https://primotechllc.com", 
 	description:
 		"Looking for wholesale prices on Uniarch products? We offer top-rated surveillance equipment for businesses and resellers",
 	type: "website",
 	image: {
-		url: "https://lovosis.com/social.png",
-    		alt: "Lovosis Technology L.L.C U.A.E", 
+		url: "https://primotechllc.com/social.png",
+    		alt: "PrimoTech LLC", 
 		width: 1200,
 		height: 630,
 	},
-	siteName: "Lovosis Technology U.A.E|Security system supplier",
+	siteName: "PrimoTech Technology U.A.E|Security system supplier",
 	openGraph: {
 		basic: {
 			title: "Top Wholesale Deals on Uniarch Security Products",
 			type: "Website",
 			image: {
-				url: "https://lovosis.com/social.png", 
-				alt: "Lovosis Technology L.L.C U.A.E", 
+				url: "https://primotechllc.com/social.png", 
+				alt: "PrimoTech LLC", 
 				width: 1200,
 				height: 630,
 			},
 		},
 		optional: {
-			siteName: "lovosis.com",
+			siteName: "primotechllc.com",
 			description:
 				"Top Wholesale Deals on Uniarch Security Products",
 			video: {
 				url: "https://youtu.be/iXoD66f3-EI",
-				alt: "Lovosis Technology L.L.C U.A.E",
+				alt: "PrimoTech LLC",
 				width: 1280,
 				height: 720,
 			},
@@ -43,12 +43,12 @@ export const SITE = {
 
 	twitter: {
 		card: "summary_large_image",
-		site: "lovosis.com",
+		site: "primotechllc.com",
 		creator: "Digiimpact",
-		title: "Top Wholesale Deals on UNV, Hikvision & Dahua Security Products",
+		title: "Top Wholesale Deals on Uniarch Security Products",
 		description:
-			"Looking for wholesale prices on UNV, Hikvision, or Dahua security products? We offer top-rated surveillance equipment for businesses and resellers",
+			"Looking for wholesale prices on Uniarch security products? We offer top-rated surveillance equipment for businesses and resellers",
 		image: "https://lovosis.com/social.png",
-		imageAlt: "Lovosis Technology L.L.C U.A.E",
+		imageAlt: "PrimoTech LLC",
 	},
 };
